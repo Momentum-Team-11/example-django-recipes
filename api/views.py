63223da1c59009ee8d53e628bc1613ee6197cc76
Amy Gori <amydgori@gmail.com>
2022-03-29 18:27:31 -0400

    Refactor to use generic view

--- api/views.py
+++ api/views.py
@@ -2,14 +2,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from core.models import Recipe
 from .serializers import RecipeSerializer
+from rest_framework.generics import ListAPIView
 
+# class RecipeListView(APIView):
+#     def get(self, request, format=None):
+#         """
+#         List all the recipes
+#         """
+#         recipes = Recipe.objects.all()
+#         serializer = RecipeSerializer(recipes, many=True)
+#         # return a response with json data
+#         return Response(serializer.data)
 
-class RecipeListView(APIView):
-    def get(self, request, format=None):
-        """
-        List all the recipes
-        """
-        recipes = Recipe.objects.all()
-        serializer = RecipeSerializer(recipes, many=True)
-        # return a response with json data
-        return Response(serializer.data)
+# refactor that view to use a Generic View
+# https://www.django-rest-framework.org/api-guide/generic-views/#listapiview
+# notice that with this I do not need to provide a get() method!
+class RecipeListView(ListAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
